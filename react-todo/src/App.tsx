39af8a2a856746c1d9b38f6b953281465d5c1b25
ssiddiqa <siddiqasayma@gmail.com>
2024-11-19 19:42:50 +0600

    local storage

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
 import TodoSummary from "./components/TodoSummary";
+import { Todo } from "./types/todo";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+    return savedTodos.length > 0 ? savedTodos : dummyData;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   function setTodoCompleted(id: number, completed: boolean) {
     setTodos((prevTodos) => prevTodos.map(todo => (
