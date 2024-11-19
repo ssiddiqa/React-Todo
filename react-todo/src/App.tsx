2377c5a4f34c26f078041ea4d711d9605b6f88e1
ssiddiqa <siddiqasayma@gmail.com>
2024-11-19 18:56:51 +0600

    delete todo

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
-import TodoItem from "./components/Todoitem"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -21,20 +21,19 @@ function App() {
       ...prevTodos
     ])
   }
+  function deleteTodo(id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <div className="space-y-2">
-          <AddTodoForm
-            onSubmit={addTodo} />
-          {todos.map(todo => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted} />
-          ))}
-        </div>
+        <AddTodoForm
+          onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo} />
       </div>
     </main>
   )
